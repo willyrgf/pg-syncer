41b68f8be2be8ec094615d8552c4d4347f77d024
Willy Romão <willyr.goncalves@gmail.com>
2020-10-03 22:10:56 +0000

    create and release conns

--- syncer/service.go
+++ syncer/service.go
@@ -25,5 +25,22 @@ func NewService(ss Syncer, ds Syncer, a Access) *Service {
 // Run the service
 func (s *Service) Run(ctx context.Context) error {
 	log.Debugf("service.Run(ctx): ctx=%+v, s=%+v", ctx, s)
+
+	sourceConn, err := s.SourceSyncer.Repo.GetConn()
+	if err != nil {
+		log.Errorf("service.Run(): s.SourceSyncer.Repo.GetConn() error=%w", err)
+		return err
+	}
+	defer sourceConn.Conn.Release()
+
+	destinationConn, err := s.DestinationSyncer.Repo.GetConn()
+	if err != nil {
+		log.Errorf("service.Run(): s.SourceSyncer.Repo.GetConn() error=%w", err)
+		return err
+	}
+	defer destinationConn.Conn.Release()
+
+	log.Debugf("service.Run(): sourceConn=%+v; destinationConn=%+v", sourceConn, destinationConn)
+
 	return nil
 }
