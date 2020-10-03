e8ee725c0defb6aba2e33738fabea83b6e8ebf7d
Willy Romão <willyr.goncalves@gmail.com>
2020-10-03 18:13:22 +0000

    move from jasonlvhit/gocron to newgo-co-op/gocron

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cryp-com-br/pg-syncer
 go 1.14
 
 require (
+	github.com/go-co-op/gocron v0.3.1
 	github.com/jackc/pgx/v4 v4.8.1
 	github.com/jasonlvhit/gocron v0.0.1
 	github.com/sirupsen/logrus v1.6.0
--- syncer/scheduler.go
+++ syncer/scheduler.go
@@ -2,29 +2,27 @@ package syncer
 
 import (
 	"errors"
+	"time"
 
-	"github.com/jasonlvhit/gocron"
+	"github.com/go-co-op/gocron"
 )
 
 // GetScheduler translate the syncersaccess
-func (a *Access) GetScheduler() (scheduler *gocron.Job, err error) {
+func (a *Access) GetScheduler() (scheduler *gocron.Scheduler, err error) {
+	scheduler = gocron.NewScheduler(time.Local)
 	switch a.PeriodicityUnit {
 	case "second", "seconds":
-		scheduler = gocron.Every(a.PeriodicityValue).Seconds()
+		scheduler.Every(a.PeriodicityValue).Seconds()
 	case "minute", "minutes":
-		scheduler = gocron.Every(a.PeriodicityValue).Minutes()
+		scheduler.Every(a.PeriodicityValue).Minutes()
 	case "hour", "hours":
-		scheduler = gocron.Every(a.PeriodicityValue).Hours()
+		scheduler.Every(a.PeriodicityValue).Hours()
 	case "day", "days":
-		scheduler = gocron.Every(a.PeriodicityValue).Days()
+		scheduler.Every(a.PeriodicityValue).Days()
 	case "week", "weeks":
-		scheduler = gocron.Every(a.PeriodicityValue).Weeks()
+		scheduler.Every(a.PeriodicityValue).Weeks()
 	default:
 		err = errors.New("the access PeriodicityUnit cannot be translated to a scheduler")
-	}
-
-	if err == nil {
-		err = scheduler.Err()
 	}
 
 	return
