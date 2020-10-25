f54ea2320faa8c7f777b5ed53e7b8289ecdc64d7
Willy Romão <willyr.goncalves@gmail.com>
2020-10-25 13:52:46 +0000

    improve logs

--- syncer/service.go
+++ syncer/service.go
@@ -46,7 +46,6 @@ func (s *Service) Run(ctx context.Context) error {
 	switch s.Access.SyncMode {
 	case FullSync:
 		log.Debugf("sync_mode selected: %s", FullSync)
-
 		err = s.truncateTable(ctx, destinationConn, s.Access.DestinationSchema, s.Access.DestinationTable)
 		if err != nil {
 			log.Errorf("service.Run(): s.truncateTable()  error=%w", err)
@@ -58,6 +57,8 @@ func (s *Service) Run(ctx context.Context) error {
 			log.Errorf("service.Run(): s.copyFromSelect()  error=%w", err)
 			return err
 		}
+		log.Debugf("finish the job, sync_mode selected: %s", FullSync)
+
 	default:
 		log.Errorf("the sync_mode configured is not implemented yet: %s", s.Access.SyncMode)
 		return fmt.Errorf("the sync_mode configured is not implemented yet: %s", s.Access.SyncMode)
