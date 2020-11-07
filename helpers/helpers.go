0d30731e70f29aa00befd92241c7fbe90744413f
Willy Romão <willyr.goncalves@gmail.com>
2020-11-07 19:48:57 +0000

    add string slices unordered checker

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"crypto/sha256"
 	"fmt"
+	"sort"
 )
 
 // ToSha256 struct to sha256 hash
@@ -11,4 +12,24 @@ func ToSha256(o interface{}) string {
 	h.Write([]byte(fmt.Sprintf("%v", o)))
 
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// ArraysIsEqual return true if both arrays have same content
+func ArraysIsEqual(t1 []string, t2 []string) bool {
+	a1 := make([]string, len(t1))
+	a2 := make([]string, len(t2))
+	copy(a1, t1)
+	copy(a2, t2)
+	sort.Strings(a1)
+	sort.Strings(a2)
+
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i, v := range a1 {
+		if v != a2[i] {
+			return false
+		}
+	}
+	return true
 }
